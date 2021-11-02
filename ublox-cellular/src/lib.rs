6640568d9c963c71395a21696cccb68bc662808e
Andres Vahter <andres@vahter.me>
2021-11-02 21:42:21 +0200

    lib: introduce new Clock trait and Instant alias

--- ublox-cellular/src/lib.rs
+++ ublox-cellular/src/lib.rs
@@ -24,6 +24,26 @@ pub use services::data::DataService;
 // Re-export atat version in use
 pub use atat;
 
+pub type Instant<const FREQ_HZ: u32> = fugit::TimerInstantU32<FREQ_HZ>;
+
+#[derive(Debug, PartialEq)]
+pub enum ClockError {
+    Infallible,
+}
+
+/// `Clock` provides all timing capabilities that are needed for the library.
+///
+/// Notice that `Clock` trait uses [fugit](https://lib.rs/crates/fugit) crate for `Duration` and `Instant`.
+pub trait Clock<const FREQ_HZ: u32> {
+    fn now(&mut self) -> fugit::TimerInstantU32<FREQ_HZ>;
+
+    fn start<T>(&mut self, count: T) -> Result<(), ClockError>
+    where
+        T: Into<fugit::MillisDurationU32>;
+
+    fn wait(&mut self) -> Result<(), ClockError>;
+}
+
 /// Prelude - Include traits
 pub mod prelude {
     #[cfg(any(feature = "socket-udp", feature = "socket-tcp"))]
