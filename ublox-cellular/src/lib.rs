914260883e2cf16ece20942f8acd425538200fe8
Andres Vahter <andres@vahter.me>
2021-11-06 17:16:01 +0200

    add some general documentation about the crate

--- ublox-cellular/src/lib.rs
+++ ublox-cellular/src/lib.rs
@@ -1,5 +1,80 @@
 #![cfg_attr(not(test), no_std)]
 
+//! # U-blox cellular
+//!
+//! This crate supports various U-blox cellular modules that are using AT commands based interface.
+//! It can be used both on `no_std` and `std` platforms.
+//!
+//! ## Example
+//!
+//! By default following features are enabled: `toby-r2`, `socket-udp`, `socket-tcp`.
+//!
+//! An example to use a different modem with only enabling TCP support:
+//!
+//! ```toml
+//! ublox-cellular-rs = { version = "0.4", default-features = false, features = ["sara-g3", "socket-tcp"] }
+//! ```
+//!
+//! ### Clock trait
+//!
+//! To use this crate one must implement [`Clock`][clock] trait for a timer.
+//! Notice that `Clock` uses [`Duration`][duration] and [`Instant`][instant] from [fugit] crate.
+//!
+//! Here is an example how it would look like for a `std` platform:
+//!
+//! ```
+//! use ublox_cellular::fugit;
+//! use ublox_cellular::prelude::*;
+//!
+//! pub struct SysTimer<const TIMER_HZ: u32> {
+//!     start: std::time::Instant,
+//!     duration: fugit::TimerDurationU32<TIMER_HZ>,
+//! }
+//!
+//! impl<const TIMER_HZ: u32> SysTimer<TIMER_HZ> {
+//!     pub fn new() -> Self {
+//!         Self {
+//!             start: std::time::Instant::now(),
+//!             duration: fugit::TimerDurationU32::millis(0),
+//!         }
+//!     }
+//! }
+//!
+//! impl<const TIMER_HZ: u32> Clock<TIMER_HZ> for SysTimer<TIMER_HZ> {
+//!     type Error = std::convert::Infallible;
+//!
+//!     fn now(&mut self) -> fugit::TimerInstantU32<TIMER_HZ> {
+//!         let millis = self.start.elapsed().as_millis();
+//!         fugit::TimerInstantU32::from_ticks(millis as u32)
+//!     }
+//!
+//!     fn start(&mut self, duration: fugit::TimerDurationU32<TIMER_HZ>) -> Result<(), Self::Error> {
+//!         self.start = std::time::Instant::now();
+//!         self.duration = duration.convert();
+//!         Ok(())
+//!     }
+//!
+//!     fn wait(&mut self) -> nb::Result<(), Self::Error> {
+//!         if std::time::Instant::now() - self.start
+//!             > std::time::Duration::from_millis(self.duration.ticks() as u64)
+//!         {
+//!             Ok(())
+//!         } else {
+//!             Err(nb::Error::WouldBlock)
+//!         }
+//!     }
+//! }
+//! ```
+//!
+//! ### Driver usage
+//!
+//! Modem driver usage examples can be found [here](https://github.com/BlackbirdHQ/ublox-cellular-rs/tree/master/examples).
+//!
+//! [clock]: prelude/trait.Clock.html
+//! [duration]: ../fugit/duration/struct.Duration.html
+//! [instant]: ../fugit/instant/struct.Instant.html
+//!
+
 mod client;
 pub mod command;
 mod config;
